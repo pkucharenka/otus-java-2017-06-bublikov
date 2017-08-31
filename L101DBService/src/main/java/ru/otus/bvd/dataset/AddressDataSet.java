782165c658e68a04227c9e39d31e56382f3034fc
Vadim Bublikov <v.bublikov@geoscan.aero>
2017-08-31 18:34:57 +0300

    Выполнено задание 10 DBService on Hibernate

---
+++ L101DBService/src/main/java/ru/otus/bvd/dataset/AddressDataSet.java
@@ -0,0 +1,39 @@
+package ru.otus.bvd.dataset;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Z_ADDRESS")
+public class AddressDataSet extends DataSet {
+    @Column(name="STREET")
+    private String street;
+    
+    @Column (name="ZIP")
+    private int zip;
+    
+    public AddressDataSet(String street, int zip) {
+        this.street = street;
+        this.zip = zip;
+    }
+    public AddressDataSet() {}
+
+    public String getStreet() {
+        return street;
+    }
+    public int getZip() {
+        return zip;
+    }
+    @Override
+    public String toString() {
+        return "AddressDataSet{" +
+               "zip=" + getZip() +
+               ",street=" + getStreet() +
+               "}";
+    };
+    
+    
+    
+    
+}
