4d75636a831de72311c649c816008007fbe4ecc9
Vadim Bublikov <v.bublikov@geoscan.aero>
2017-09-19 12:11:30 +0300

    Inject значений в поля сервлета изменен на @Autowired

--- L13DBService_IoC/src/main/java/ru/otus/bvd/servlet/AdminServlet.java
+++ L13DBService_IoC/src/main/java/ru/otus/bvd/servlet/AdminServlet.java
@@ -1,24 +1,28 @@
 package ru.otus.bvd.servlet;
 
-import org.springframework.context.ApplicationContext;
-import ru.otus.bvd.base.DBService;
-import ru.otus.bvd.cache.CacheEngine;
-import ru.otus.bvd.cache.CacheEngineAdmin;
-import ru.otus.bvd.example.DBActivity;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import ru.otus.bvd.base.DBService;
+import ru.otus.bvd.cache.CacheEngine;
+import ru.otus.bvd.cache.CacheEngineAdmin;
+import ru.otus.bvd.example.DBActivity;
 
 /**
  * Created by tully.
  */
+@Component
 public class AdminServlet extends HttpServlet {
 
     private static final String DEFAULT_USER_NAME = "UNKNOWN";
@@ -30,17 +34,18 @@ public class AdminServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
 
-        Map<String, Object> pageVariables = createCacheVariablesMap();
-        //let's get login from session
         String login = (String) request.getSession().getAttribute(LoginServlet.LOGIN_PARAMETER_NAME);
-        pageVariables.put("login", login != null ? login : DEFAULT_USER_NAME);
-        pageVariables.put(REFRESH_VARIABLE_NAME, String.valueOf(PERIOD_MS));
-        
-        if (!pageVariables.get("login").equals("admin")) {
+
+        if (!login.equals("admin")) {
             response.setContentType("text/html;charset=utf-8");
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);      
             return;
         }
+
+        //let's get login from session
+        Map<String, Object> pageVariables = createCacheVariablesMap();
+        pageVariables.put("login", login != null ? login : DEFAULT_USER_NAME);
+        pageVariables.put(REFRESH_VARIABLE_NAME, String.valueOf(PERIOD_MS));
         
         response.getWriter().println(TemplateProcessor.instance().getPage(ADMIN_PAGE_TEMPLATE, pageVariables));
 
@@ -48,8 +53,9 @@ public class AdminServlet extends HttpServlet {
         response.setStatus(HttpServletResponse.SC_OK);
     }
 
-    private CacheEngineAdmin cacheEngineAdmin;
-    private DBService dbService;
+    @Autowired private CacheEngineAdmin cacheEngineAdmin;
+    @Autowired private DBService dbService;
+    
     private Map<String, Object> createCacheVariablesMap() {
         Map<String, Object> cacheVariables = new HashMap<>();
 
@@ -69,13 +75,17 @@ public class AdminServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        ApplicationContext springContext = (ApplicationContext) config.getServletContext().getAttribute("springContext");
-        cacheEngineAdmin = (CacheEngineAdmin) springContext.getBean("cacheEngine");
-        dbService = (DBService) springContext.getBean("dbService");
-
+        
+        initAutowiredBeans();
+        
         Thread dbActivity = new Thread( new DBActivity(dbService));
         dbActivity.start();
 
     }
+    
+    private void initAutowiredBeans() {
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
+    
 
 }
