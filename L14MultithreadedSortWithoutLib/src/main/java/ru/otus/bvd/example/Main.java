ee34c69c70ac0f70edb6615ff33297910f21962b
Vadim Bublikov <v.bublikov@geoscan.aero>
2017-09-27 13:48:06 +0300

    Поработал над качеством кода

--- L14MultithreadedSortWithoutLib/src/main/java/ru/otus/bvd/example/Main.java
+++ L14MultithreadedSortWithoutLib/src/main/java/ru/otus/bvd/example/Main.java
@@ -7,31 +7,43 @@ import java.util.logging.Logger;
 import ru.otus.bvd.example.sorter.Sorter;
 
 public class Main {
-    public static final Logger log = Logger.getLogger(Main.class.getName());
+    private static final Logger log = Logger.getLogger(Main.class.getName());
     
     public static void main(String[] args) throws InterruptedException {
         configureLog();
         
-        Sorter sorter = new Sorter( 4 );    
+        Sorter sorter = new Sorter(4);    
         int[] data = makeData();        
+        //int[] data = new int[] {4,0,5,5,Integer.MAX_VALUE,9,5,8};
+        
+        int unsortedDataLength = data.length;
         
         sorter.setUnsortedData(data);
         sorter.schedule();
         sorter.join();
         data = sorter.getSortedData();
 
-        for (int i = 0; i < 50; i++) {
-            log.info( Integer.toString(data[i]) );             
+        if (data.length<100) {
+            for (int i = 0; i < data.length; i++) {
+                if (log.isLoggable(Level.INFO)) log.info( Integer.toString(i) + " = " + data[i] );             
+            }            
+        } else {
+            for (int i = 0; i < 50; i++) {
+                if (log.isLoggable(Level.INFO)) log.info( Integer.toString(i) + " = " + data[i] );             
+            }
+            log.info("...");
+            for (int i = data.length-51; i < data.length; i++) {
+                if (log.isLoggable(Level.INFO)) log.info( Integer.toString(i) + " = " + data[i]  );             
+            }            
         }
-        log.info("...");
-        for (int i = data.length-51; i < data.length; i++) {
-            log.info( Integer.toString(data[i]) );             
-        }
+        
+        if (log.isLoggable(Level.INFO)) log.info("Summary: sorted data length=" + data.length + 
+                ", unsorted data length=" + unsortedDataLength);
     }
     
     private static void configureLog() {
         System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tF %1$tT %4$s %2$s %5$s%6$s%n");
-        log.setLevel(Level.ALL);
+        log.setLevel(Level.INFO);
         
     }
 
--- L14MultithreadedSortWithoutLib/src/main/java/ru/otus/bvd/example/sorter/Sorter.java
+++ L14MultithreadedSortWithoutLib/src/main/java/ru/otus/bvd/example/sorter/Sorter.java
@@ -1,69 +1,77 @@
 package ru.otus.bvd.example.sorter;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Sorter {
-    private final int countThread;
-    private final SortWorker[] threadPool;
+    private static final Logger log = Logger.getLogger(Sorter.class.getName());
+    
+    private final SortWorker[] workers;
     private int[] data;
     private Thread splitThread;
     
     public Sorter(int countThread) {
-        this.countThread = countThread;
-        this.threadPool = new SortWorker[countThread];
-        for (int i = 0; i < threadPool.length; i++) {
-            threadPool[i] = new SortWorker();
+        this.workers = new SortWorker[countThread];
+        for (int i = 0; i < workers.length; i++) {
+            workers[i] = new SortWorker();
         }
     }
     
     public void setUnsortedData(int[] data) {
-        this.data = data;
-        
-        
+        this.data = data;        
     }
+    
     public int[] getSortedData() {
         return data;
     }
     
     public void schedule() {
         splitThread = new Thread( () -> {
-            int chunkSize = data.length/threadPool.length;            
-            for (int i = 0; i < threadPool.length; i++) {
-                threadPool[i].setUnSortedData( Arrays.copyOfRange(data, chunkSize * i, chunkSize * (i+1)) );
-                threadPool[i].start();
+            Thread currentThread = Thread.currentThread();
+            int dataLength = data.length;
+            
+            double chunkSizeDouble = ( (double) dataLength)/workers.length;  
+            int chunkSize = BigDecimal.valueOf((chunkSizeDouble)).setScale(0, RoundingMode.UP).intValueExact();            
+            
+            if (log.isLoggable(Level.CONFIG)) log.info("Chunk size = " + chunkSize);
+            for (int i = 0; i < workers.length; i++) {                
+                int[] chunkData = Arrays.copyOfRange(data, chunkSize * i, Math.min(chunkSize*(i+1),dataLength));
+                if (log.isLoggable(Level.CONFIG)) log.config(Arrays.toString(chunkData));
+                workers[i].setUnSortedData( chunkData );
+                workers[i].start();
             }    
-            for (int i = 0; i < threadPool.length; i++) {
+            for (int i = 0; i < workers.length && !currentThread.isInterrupted() ; i++) {
                 try {
-                    threadPool[i].join();
+                    workers[i].join();
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    currentThread.interrupt();
                 }
             }
-            for (int i = 0; i < data.length; i++) {
-                data[i] = Integer.MAX_VALUE;
+            if (currentThread.isInterrupted())
+                return;
+            for (int i = 0; i < dataLength ; i++) {
+                int minElement = Integer.MAX_VALUE;
                 int selectedChunk=-1;
-                for (int t = 0; t < threadPool.length; t++) {                    
-                    if (threadPool[t].hasNext() &&threadPool[t].getCurrentElement()<data[i]) {
-                        data[i] = threadPool[t].getCurrentElement();
+                for (int t = 0; t < workers.length; t++) {                    
+                    if (workers[t].hasNext() &&workers[t].getCurrentElement()<minElement) {
+                        minElement = workers[t].getCurrentElement();
                         selectedChunk = t;
                     }
                 }
-                if (selectedChunk == -1) {
-                    System.out.println("Здесь нужно что-то делать");
-                } else {
-                    threadPool[selectedChunk].currentPositionIncrement();
-                }
+                data[i] = minElement;
+                if (selectedChunk != -1)
+                    workers[selectedChunk].currentPositionIncrement();
             }
         });
-
         
-        splitThread.start();
-        
+        splitThread.start();        
     }
-    
+        
     public void join() throws InterruptedException {
         splitThread.join();
     }
-    
     
 }
